4dd1a34719426915e3e72c985fb10ed6df8734e6
tetourne <thomas.etourneau@tutanota.com>
2023-05-03 17:02:04 +0200

    Add tuto_hands_on_ML folders and scripts from exercises of chapter 2 and 3.

---
+++ tuto_hands_on_ML/chapter_2/main.py
@@ -0,0 +1,262 @@
+import functions as fn
+import transformers as tr
+import pandas as pd
+import numpy as np
+import scipy as sp
+import joblib
+from matplotlib import pyplot as plt
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer, make_column_selector, make_column_transformer
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import mean_squared_error
+
+
+########## SETUP ##########
+visualize_data = False
+train_model = False
+do_grid_search = False
+do_rnd_search = False
+
+
+########## Read and split data ##########
+filename = "../handson-ml/datasets/housing/housing.csv"
+housing, test_set = fn.read_and_split_data(filename)
+
+########## Visualize data ##########
+if visualize_data:
+    # scatter matrix
+    attributes = housing.columns
+    # attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+    pd.plotting.scatter_matrix(housing[attributes], figsize=(12, 8))
+    fn.save_fig("scatter_matrix_plot")  # extra code
+    plt.show()
+    
+    # correlations
+    corr_matrix = housing.corr()
+    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+    
+    # Attribute combinations
+    housing["rooms_per_house"] = housing["total_rooms"] / housing["households"]
+    housing["bedrooms_ratio"] = housing["total_bedrooms"] / housing["total_rooms"]
+    housing["people_per_house"] = housing["population"] / housing["households"]
+    corr_matrix = housing.corr()
+    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+
+########## Clean Data ##########
+# Get instances and labels
+housing_labels = housing["median_house_value"].copy()
+housing.drop("median_house_value", axis=1, inplace=True)
+
+# Split numerical and categorical data
+housing_num = housing.select_dtypes(include=[np.number])
+housing_cat = housing.select_dtypes(include=[object])
+num_attribs = list(housing_num)
+cat_attribs = list(housing_cat)
+
+# # num transformers
+# selector_num = tr.DataFrameSelector(num_attribs)
+# imputer = SimpleImputer(strategy="median")
+# attribs_adder = tr.CombinedAttributesAdder()
+# std_scaler = StandardScaler()
+# # cat transformers
+# selector_cat = tr.DataFrameSelector(cat_attribs)
+# one_hot_encoder = OneHotEncoder(sparse_output=False)
+
+# # Apply num transformers manually
+# h1_num = selector_num.fit_transform(housing)
+# h2_num = imputer.fit_transform(h1_num)
+# h3_num = attribs_adder.fit_transform(h2_num)
+# h4_num = std_scaler.fit_transform(h3_num)
+# # Apply cat transformers
+# h1_cat = selector_cat.fit_transform(housing)
+# h2_cat = one_hot_encoder.fit_transform(h1_cat)
+# h3_cat = pd.DataFrame(h2_cat, columns=one_hot_encoder.get_feature_names_out())
+
+### There is an example in the jupyter notebook that shows how to create a transformer
+### which identifies the clusters in (Latitude, Longitude) (districts) where the
+### median house value is the highest (using sklearn.cluster.KMeans)
+
+# Create pipeline with all transformers
+# num_pipeline = Pipeline([
+#     # ('selector', tr.DataFrameSelector(num_attribs)), ### replaced by ColumnTransformer
+#     ('imputer', SimpleImputer(strategy="median")),
+#     ('attribs_adder', tr.CombinedAttributesAdder()),
+#     ('std_scaler', StandardScaler()),
+# ])
+# num_pipeline = make_pipeline(  ### make_pipeline is an alternative to Pipeline
+#     tr.DataFrameSelector(num_attribs),
+#     SimpleImputer(strategy="median"),
+#     tr.CombinedAttributesAdder()
+# #     StandardScaler()
+# )
+# cat_pipeline = Pipeline([
+#     # ('selector', tr.DataFrameSelector(cat_attribs)),  ### replaced by ColumnTransformer
+#     ('imputer', SimpleImputer(strategy="most_frequent")),
+#     ('onehotencoder', OneHotEncoder(sparse_output=False)),
+# ])
+# cat_pipeline = make_pipeline( ### alternative to Pipeline
+#     tr.DataFrameSelector(cat_attribs),
+# #     LabelBinarizer()
+# )
+
+# preprocessing = ColumnTransformer([  ### Alternative to FeatureUnion
+#     ("num", num_pipeline, num_attribs),
+#     ("cat", cat_pipeline, cat_attribs),
+# ])
+# preprocessing = make_column_transformer(  ### Alternative to ColumnTransformer
+#     (num_pipeline, make_column_selector(dtype_include=np.number)),
+#     (cat_pipeline, make_column_selector(dtype_include=object)),
+# )
+# housing_prepared = preprocessing.fit_transform(housing)
+
+# preprocessing = FeatureUnion(transformer_list=[  ### Combine num and cat pipeline ; need DataFrameSelector in each pipeline
+#     ("num_pipeline", num_pipeline),
+#     ("cat_pipeline", cat_pipeline),
+# ])
+# housing_prepared = preprocessing.fit_transform(housing)
+
+### A more complexe pipeline that allows to use pipeline.get_feature_names_out()
+log_pipeline = make_pipeline(  # pipeline which takes the log of a num series: it's correcting for the skewed distributions (making them more symetric)
+    SimpleImputer(strategy="median"),
+    FunctionTransformer(np.log, feature_names_out="one-to-one"),
+    StandardScaler())
+default_num_pipeline = make_pipeline(  # use as the default pipeline in ColumnTransformer()
+    SimpleImputer(strategy="median"), 
+    StandardScaler())
+cat_pipeline = make_pipeline(  # pipeline for categorical series
+    SimpleImputer(strategy="most_frequent"),
+    OneHotEncoder(sparse_output=False))
+preprocessing = ColumnTransformer([
+        ("bedrooms", fn.ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+        ("rooms_per_house", fn.ratio_pipeline(), ["total_rooms", "households"]),
+        ("people_per_house", fn.ratio_pipeline(), ["population", "households"]),
+        ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population",
+                               "households", "median_income"]),
+        # ("geo", cluster_simil, ["latitude", "longitude"]), not implemented, the code is in the notebook
+        ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+    ],
+    remainder=default_num_pipeline)  # one column remaining: housing_median_age. If no transformer specified, the default is to drop the columns
+housing_prepared = preprocessing.fit_transform(housing)
+df_housing_prepared = pd.DataFrame(
+    housing_prepared,
+    columns=preprocessing.get_feature_names_out(),
+    index=housing.index)  # looks like the indices are shuffled if index is not specified
+
+
+########## Select and train models ##########
+### The fitting of model can be include into a pipeline that does the transformation
+### of data and then the fitting of the model :
+### lin_reg = make_pipeline(preprocessing, LinearRegression())
+### lin_reg.fit(housing, housing_labels)
+
+# Linear regressor
+lin_reg = LinearRegression()
+if train_model:
+    print("Training Linear Regressor")
+    fn.eval_model(lin_reg, housing_prepared, housing_labels)
+
+# Decision Tree regressor
+tree_reg = DecisionTreeRegressor()
+if train_model:
+    print("Training Decision Tree Regressor")
+    fn.eval_model(tree_reg, housing_prepared, housing_labels)
+
+# Random Forest regressor
+forest_reg = RandomForestRegressor()
+if train_model:
+    print("Training Random Forest Regressor")
+    fn.eval_model(forest_reg, housing_prepared, housing_labels)
+
+### Use GridSearchCV to fine tune the models
+param_grid = [
+    {'n_estimators': [10, 30, 100], 'max_features': [6, 8, 10, 12]},
+    {'bootstrap': [False], 'n_estimators': [10, 30], 'max_features': [4, 8, 12]},
+]
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_root_mean_squared_error')
+if do_grid_search:
+    print("Doing GridSearchCV with:")
+    print(param_grid)
+    grid_search.fit(housing_prepared, housing_labels)
+    grid_search.best_params_
+    grid_search.best_estimator_
+    fn.display_cv_results(grid_search)
+    fn.display_feature_importances(grid_search, df_housing_prepared.columns)
+
+### Randomized Search
+# better to use randomized search when the hyperparameter space is large
+
+param_distrib = {'n_estimators': sp.stats.randint(low=2, high=120),
+                 'max_features': sp.stats.randint(low=2, high=20)}
+rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distrib,
+                                n_iter=20, cv=5,
+                                scoring='neg_root_mean_squared_error', random_state=42)
+if do_rnd_search:
+    print("Doing RandomizedSearchCV with:")
+    print(param_distrib)
+    rnd_search.fit(housing_prepared, housing_labels)
+    rnd_search.best_params_
+    rnd_search.best_estimator_
+    fn.display_cv_results(rnd_search)
+    fn.display_feature_importances(rnd_search, df_housing_prepared.columns)
+
+### These are other random generators that can be use to define the param_distributions
+# scipy.stats.randint(a, b+1): for hyperparameters with discrete values that range from a to b, and all values in that range seem equally likely.
+# scipy.stats.uniform(a, b): this is very similar, but for continuous hyperparameters.
+# scipy.stats.geom(1 / scale): for discrete values, when you want to sample roughly in a given scale. E.g., with scale=1000 most samples will be in this ballpark, but ~10% of all samples will be <100 and ~10% will be >2300.
+# scipy.stats.expon(scale): this is the continuous equivalent of geom. Just set scale to the most likely value.
+# scipy.stats.reciprocal(a, b): when you have almost no idea what the optimal hyperparameter value's scale is. If you set a=0.01 and b=100, then you're just as likely to sample a value between 0.01 and 0.1 as a value between 10 and 100.
+
+### Select and save best model
+# final_model = rnd_search.best_estimator_
+final_model = RandomForestRegressor(bootstrap=False, max_features=6, n_estimators=200)  # got from rnd search + grid search
+final_model.fit(housing_prepared, housing_labels)  # only needed if the model is redefined the line above
+print("Evaluating final model on training set...")
+fn.eval_model(final_model, housing_prepared, housing_labels)  # last eval before comparing with test set
+
+# build full pipeline and save
+final_pipeline = Pipeline([
+    ("preprocessing", preprocessing),
+    ("random_forest", final_model),
+    ])
+joblib.dump(final_pipeline, "Models/final_pipeline.pkl")
+
+########## Evaluate the model on the test set ##########
+X_test = test_set.drop("median_house_value", axis=1)
+y_test = test_set["median_house_value"].copy()
+
+# # Transform test set
+# X_test_prepared = preprocessing.transform(X_test)
+# df_X_test_prepared = pd.DataFrame(  # Use DataFrame if necessary
+#     X_test_prepared,
+#     columns=preprocessing.get_feature_names_out(),
+#     index=X_test.index)  # looks like the indices are shuffled if index is not specified
+# # ... and make Prediction
+# final_predictions = final_model.predict(X_test_prepared)
+
+# Or directly make prediction
+final_predictions = final_pipeline.predict(X_test)
+
+# Evaluate model
+print("Evaluating final model on test set...")
+final_rmse = mean_squared_error(y_test, final_predictions, squared=False)
+print("Final RMSE on test set is: {}".format(final_rmse))
+confidence = 0.95
+squared_errors = (final_predictions - y_test) ** 2
+print("{}% confidence interval on RMSE is:".format(confidence*100))
+print(np.sqrt(sp.stats.t.interval(confidence, len(squared_errors) - 1,
+                         loc=squared_errors.mean(),
+                         scale=sp.stats.sem(squared_errors))))
+
+
+########## Reload the model and make prediction on new data ##########
+final_pipeline_reloaded = joblib.load("Models/final_pipeline.pkl")
+new_data = housing.iloc[:5]  # pretend these are new districts
+predictions = final_pipeline_reloaded.predict(new_data)
---
+++ tuto_hands_on_ML/chapter_3/main.py
@@ -0,0 +1,187 @@
+import sys
+sys.path.append('/home/thomas/Documents/Python/Libraries/')
+import functions as fn
+import numpy as np
+import joblib
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score, cross_val_predict, GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, precision_recall_curve, roc_curve, roc_auc_score
+
+
+random_forest = False
+do_grid_search = False
+do_plot = False
+
+########## Load data ##########
+mnist = fn.load_mnist()
+X = mnist.data
+y = mnist.target
+# split train and test sets
+X_train, X_test = X[:60000], X[60000:]
+y_train, y_test = y[:60000], y[60000:]
+# shuffle the training set to have similiar cross-validation folds
+shuffle_index = np.random.permutation(60000)
+X_train = X_train[shuffle_index]
+y_train = y_train[shuffle_index]
+
+
+########## Binary Classifier ##########
+# recognize digit '5'
+y_train_5 = (y_train == '5')
+y_test_5 = (y_test == '5')
+
+sgd_clf = SGDClassifier(random_state=42)
+print("Fitting SGD Classifier...")
+sgd_clf.fit(X_train, y_train_5)
+
+
+########## Cross Validation ##########
+### Accuracy:
+# how much good predictions have been done
+accuracies = cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+# accuracies = fn.cross_val_accuracy(sgd_clf, X_train, y_train_5, cv=3)  # recoding of sklearn's function
+print("Accucaries: {}".format(accuracies))
+
+### Confusion Matrix:
+# [[TN, FP],[FN, TP]]
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+cm = confusion_matrix(y_train_5, y_train_pred)
+print("Confusion matrix:")
+print(cm)
+
+### Precision and recall
+# preci: TP/(TP+FP) : ratio of good positive predictions
+# recall or sensitivity of true positive rate: TP/(TP+FN) : ratio of positive instances corretly detected by the classifier
+preci = precision_score(y_train_5, y_train_pred)
+print("Precision: {}".format(preci))
+recall = recall_score(y_train_5, y_train_pred)
+print("Recall: {}".format(recall))
+f1score = f1_score(y_train_5, y_train_pred)
+print("F1 score: {}".format(f1score))
+
+### Precision / recall trade-off:
+threshold = 3000
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+if do_plot:
+    fn.plot_preci_recall_tradeoff(precisions, recalls, thresholds, threshold=threshold)
+    fn.plot_preci_vs_recall(precisions, recalls, thresholds, threshold=threshold)
+
+### Find threshold for a given precision
+precision_mini = 90
+idx_for_precision_mini = (precisions >= precision_mini).argmax()
+threshold_for_precision_mini = thresholds[idx_for_precision_mini]
+print("Threshold to acheive {} precision: {}".format(precision_mini, threshold_for_precision_mini))
+y_train_pred_preci_mini = (y_scores >= threshold_for_precision_mini)
+print("Precision for this threshold:{}".format(precision_score(y_train_5, y_train_pred_preci_mini)))
+recall_at_precision_mini = recall_score(y_train_5, y_train_pred_preci_mini)
+print("Recall for this threshold:{}".format(recall_at_precision_mini))
+
+### ROC curve
+fpr, tpr, thresholds_roc = roc_curve(y_train_5, y_scores)
+if do_plot:
+    fn.plot_roc_curve(fpr, tpr, thresholds_roc, threshold=threshold)
+print("ROC AUC score: {}".format(roc_auc_score(y_train_5, y_scores)))
+      
+### Compare with Random Forest Classifier
+if random_forest:
+    forest_clf = RandomForestClassifier(random_state=42)
+    y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                        method="predict_proba")  # for each instance, return a probability for each class. Here it's 2
+    y_scores_forest = y_probas_forest[:, 1]  # consider the score as the probability of positive class
+    precisions_forest, recalls_forest, thresholds_forest = precision_recall_curve(y_train_5, y_scores_forest)
+    if do_plot:
+        fn.plot_preci_vs_recall(precisions, recalls, thresholds, last_curve=False)
+        fn.plot_preci_vs_recall(precisions_forest, recalls_forest, thresholds_forest)
+
+
+########## Trying KNeighbors Classifier ##########
+kneighbors_clf = KNeighborsClassifier()
+
+# explore the model hyperparameters with grid search
+param_grid = [
+    {'n_neighbors': [2, 4, 6, 8], 'weights': ['uniform', 'distance']}
+]
+grid_search = GridSearchCV(kneighbors_clf, param_grid, cv=5)
+if do_grid_search:
+    print("Doing GridSearchCV with:")
+    print(param_grid)
+    grid_search.fit(X_train, y_train)
+    joblib.dump(grid_search, "Pkls/grid_search_kneighbors_clf.pkl")
+if not do_grid_search:
+    print("Loading GridSearchCV:")
+    grid_search = joblib.load("Pkls/grid_search_kneighbors_clf.pkl")
+grid_search.best_params_
+grid_search.best_estimator_
+fn.display_cv_results(grid_search)
+# fn.display_feature_importances(grid_search)
+
+
+### Evaluate best model
+best_model = grid_search.best_estimator_
+print("Best model is:")
+print(best_model)
+
+# on train set
+print("Evaluate best model on train set:")
+y_train_pred = cross_val_predict(best_model, X_train, y_train, cv=10)
+preci = precision_score(y_train, y_train_pred, average='macro')  # The average='macro' parameter calculates the precision for each class separately and then takes the average, giving equal weight to each class.
+print("Precision: {}".format(preci))
+recall = recall_score(y_train, y_train_pred, average='macro')
+print("Recall: {}".format(recall))
+f1score = f1_score(y_train, y_train_pred, average='macro')
+print("F1 score: {}".format(f1score))
+
+# on test set
+print("Evaluate best model on test set:")
+best_model.fit(X_train, y_train)
+y_test_pred = best_model.predict(X_test)
+preci = precision_score(y_test, y_test_pred, average='macro')
+print("Precision: {}".format(preci))
+recall = recall_score(y_test, y_test_pred, average='macro')
+print("Recall: {}".format(recall))
+f1score = f1_score(y_test, y_test_pred, average='macro')
+print("F1 score: {}".format(f1score))
+
+
+########## Trying data augmentation ##########
+# Augment data with shifted images
+dx, dy = 3, 3
+X_train_augmented = [image for image in X_train]
+y_train_augmented = [label for label in y_train]
+for ddx, ddy in ((-dx, -dy), (-dx, dy), (dx, -dy), (dx, dy)):
+    for image, label in zip(X_train, y_train):
+        X_train_augmented.append(fn.shift_image(image, ddx, ddy))
+        y_train_augmented.append(label)
+X_train_augmented = np.array(X_train_augmented)
+y_train_augmented = np.array(y_train_augmented)
+
+# Shuffle the augmented training set
+shuffle_idx = np.random.permutation(len(X_train_augmented))
+X_train_augmented = X_train_augmented[shuffle_idx]
+y_train_augmented = y_train_augmented[shuffle_idx]
+
+# Evaluate best model with augmented training set
+# on train set
+print("Evaluate best model on augmented train set:")
+y_train_augmented_pred = cross_val_predict(best_model, X_train_augmented, y_train_augmented, cv=10)
+preci = precision_score(y_train_augmented, y_train_augmented_pred, average='macro')  # The average='macro' parameter calculates the precision for each class separately and then takes the average, giving equal weight to each class.
+print("Precision: {}".format(preci))
+recall = recall_score(y_train_augmented, y_train_augmented_pred, average='macro')
+print("Recall: {}".format(recall))
+f1score = f1_score(y_train_augmented, y_train_augmented_pred, average='macro')
+print("F1 score: {}".format(f1score))
+
+# on test set
+print("Evaluate best model on test set with augmented train set:")
+best_model.fit(X_train_augmented, y_train_augmented)
+y_test_pred = best_model.predict(X_test)
+preci = precision_score(y_test, y_test_pred, average='macro')
+print("Precision: {}".format(preci))
+recall = recall_score(y_test, y_test_pred, average='macro')
+print("Recall: {}".format(recall))
+f1score = f1_score(y_test, y_test_pred, average='macro')
+print("F1 score: {}".format(f1score))
