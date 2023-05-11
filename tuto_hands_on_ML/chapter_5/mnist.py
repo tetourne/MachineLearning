8032b2232872ebe005b609586fa79a8bea480aff
tetourne <thomas.etourneau@tutanota.com>
2023-05-11 14:57:59 +0200

    Final version + logs for mnist.py

--- tuto_hands_on_ML/chapter_5/mnist.py
+++ tuto_hands_on_ML/chapter_5/mnist.py
@@ -4,6 +4,7 @@ import joblib
 import numpy as np
 import scipy as sp
 from pathlib import Path
+from scipy.stats import loguniform, uniform
 from MachineLearning import functions as fn
 from sklearn.svm import SVC, LinearSVC
 from sklearn.pipeline import Pipeline
@@ -15,6 +16,7 @@ from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_
 
 do_cross_val = False
 do_grid_search = False
+do_rnd_search = False
 
 
 ########## Configure logger ##########
@@ -30,8 +32,7 @@ logger.info("Loading MNIST dataset")
 mnist = fn.load_mnist()
 X = mnist.data
 y = mnist.target
-logger.warning("test")
-stop
+
 # split train and test sets
 train_length = 60000  # 60000 for the full MNIST dataset
 X_train, X_test = X[:train_length], X[train_length:]
@@ -43,7 +44,7 @@ X_train = X_train[shuffle_index]
 y_train = y_train[shuffle_index]
 
 # select only a subset of the training set
-subset_len = 15000
+subset_len = 2000
 X_train = X_train[:subset_len]
 y_train = y_train[:subset_len]
 
@@ -93,10 +94,13 @@ svm_clf = Pipeline([
     ('standardscaler', StandardScaler()),  # necessary for SVM classifier
     ('svc', SVC())
 ])
+mod_name = "svm_clf_3"
 param_grid = [
-    {'svc__kernel': ['poly'], 'svc__gamma': [1e-2, 1, 10], 'svc__C': [0.1, 1, 10]}
+    {'svc__kernel': ['poly'], 'svc__gamma': ['scale', 0.1, 1], 'svc__C': [0.1, 1, 10]}
 ]
-grid_search = GridSearchCV(svm_clf, param_grid, cv=3)
+grid_search = GridSearchCV(svm_clf, param_grid,
+                           scoring='precision_macro',
+                           cv=3, verbose=3)
 if do_grid_search:
     logger.info("Doing GridSearchCV with:")
     logger.info(param_grid)
@@ -105,21 +109,61 @@ if do_grid_search:
     pkl_path = Path(__file__).parents[0] / "pkls"
     if not pkl_path.is_dir():
         pkl_path.mkdir(parents=True, exist_ok=True)
-    joblib.dump(grid_search, pkl_path / "grid_search_svc_clf_2.pkl")
+    joblib.dump(grid_search, pkl_path / "grid_search_{}.pkl".format(mod_name))
 if not do_grid_search:
     pkl_path = Path(__file__).parents[0] / "pkls"
     logger.info("Loading GridSearchCV:")
-    grid_search = joblib.load(pkl_path / "grid_search_svc_clf.pkl")
+    grid_search = joblib.load(pkl_path / "grid_search_{}.pkl".format(mod_name))
 logger.info(grid_search.best_params_)
 logger.info(grid_search.best_estimator_)
-fn.display_cv_results(grid_search)
+cv_res = fn.get_cv_results(grid_search)
+logger.info(cv_res)
+
+
+########## Randomized Search ##########
+svm_clf = Pipeline([
+    ('standardscaler', StandardScaler()),  # necessary for SVM classifier
+    ('svc', SVC())
+])
+mod_name = "svm_clf"
+param_distrib = {
+    'svc__kernel': ['poly'],
+    'svc__gamma': loguniform(1e-2, 1),
+    'svc__C': loguniform(0.1, 10)
+}
+# scoring methods can be accuracy, precision, recall, f1score, roc_auc...
+# By default, it uses the scoring method of the estimator. For a classifier,
+# it's the mean accuracy
+rnd_search = RandomizedSearchCV(svm_clf, param_distributions=param_distrib,
+                                n_iter=10, cv=3,
+                                scoring='roc_auc',
+                                random_state=42, verbose=3)
+if do_rnd_search:
+    logger.info("Doing RandomizedSearchCV with:")
+    logger.info(param_distrib)
+    rnd_search.fit(X_train, y_train)
+    # Save search
+    pkl_path = Path(__file__).parents[0] / "pkls"
+    if not pkl_path.is_dir():
+        pkl_path.mkdir(parents=True, exist_ok=True)
+    joblib.dump(rnd_search, pkl_path / "rnd_search_{}.pkl".format(mod_name))
+else:
+    pkl_path = Path(__file__).parents[0] / "pkls"
+    logger.info("Loading RandomizedSearchCV:")
+    rnd_search = joblib.load(pkl_path / "rnd_search_{}.pkl".format(mod_name))
+    
+logger.info(rnd_search.best_params_)
+logger.info(rnd_search.best_estimator_)
+cv_res = fn.get_cv_results(rnd_search)
+logger.info(cv_res)
+# fn.display_feature_importances(rnd_search, df_housing_prepared.columns)
 
 
 ########## Test best model ##########
 # Define best model
 final_model = Pipeline([
     ('standardscaler', StandardScaler()),
-    ('svc', SVC(C=0.1, gamma=1, kernel='poly'))
+    ('svc', SVC(C=1, gamma=1, kernel='poly'))
 ])
 
 # Read again full data (used subset to do grid_search)
